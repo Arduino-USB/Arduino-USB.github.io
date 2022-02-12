6e120adef8933ffe2cf8a31c789624421adfe1ed
Arduino-USB <91158137+Arduino-USB@users.noreply.github.com>
2022-02-12 17:04:43 -0500

    Delete backdoor directory

--- backdoor/restart.bat
+++
@@ -1,3 +0,0 @@
-taskkill /IM py.exe
-C:\Users\Arduino999\Desktop\Python\py.exe  C:\Users\Arduino999\Desktop\backdoor\server.py
-pause
--- backdoor/victim/reset.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-taskkill /IM vic.exe /F
-start C:\Windows\vic.exe
